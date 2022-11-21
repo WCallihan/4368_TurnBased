fb6b78029a6412ef9c531cb8890968e3a698700e
William <willcallihan13@gmail.com>
2022-11-21 17:31:35 -0600

    setup button fix

--- Assets/Scripts/SetupButton.cs
+++ Assets/Scripts/SetupButton.cs
@@ -1,12 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SetupButton : MonoBehaviour {
 
-    [SerializeField] private SetupController setupController;
     [SerializeField] private CharacterData characterData;
+    [SerializeField] private Image characterImage;
+    [SerializeField] private TextMeshProUGUI nameText;
     [SerializeField] private GameObject blackoutPanel;
+
+    private SetupController setupController;
+
+    private void Awake() {
+        setupController = FindObjectOfType<SetupController>();
+        SetCharacter();
+    }
+
+    private void SetCharacter() {
+        characterImage.sprite = characterData.Sprite;
+        nameText.text = characterData.Name;
+    }
 
     //called by the setup buttons and send the character data and blackout panel to the setup controller
     public void SendCharacter() {
